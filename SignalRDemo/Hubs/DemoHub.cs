016d639068fb5242212682074e7a1229cb8fed96
unknown <Ryan@Sonata.(none)>
2012-07-17 18:43:08 +0000

    Basic demo ready.

---
+++ SignalRDemo/Hubs/DemoHub.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SignalR.Hubs;
+
+namespace SignalRDemo.Hubs
+{
+    public class DemoHub : Hub
+    {
+
+        public DemoHub()
+        {
+
+        }
+
+        public void Send(string message)
+        {
+            Clients.updatePage(message);
+        }
+    }
+}
---
+++ SignalRDemo/Scripts/signalrdemo.js
@@ -0,0 +1,15 @@
+﻿var hub = $.connection.demoHub;
+
+// Declare methods for the server to call ================
+hub.updatePage = function (message) {
+    $('#messages').append(message + '</ br>');
+};
+
+// END : Declarae methods for the server to call =========
+
+$('#send').click(function () {
+    var messageToSend = $('#msg').val();
+    hub.send(messageToSend);
+});
+
+$.connection.hub.start();
